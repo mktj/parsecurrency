f0f6b22ab01b208d104850344deaa5d4bb5dbc8c
rcaziraghi <rcaziraghi@gmail.com>
2022-11-06 01:33:37 +0000

    Requested adjustments.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var currencyMatcher = /^(?:([-,+]{1}) ?)?(?:(\p{Sc}{1,5}) ?)?(?:([A-Z]{3}) ?)?(?:([^\d ]+?) ?)?(((?:\d{1,3}([,. ’'\u00A0]))*?\d{1,})(([,.])\d{1,2})?)(?: ?([^\d ]+?))??(?: ?([A-Z]{3}))?$/;
+var currencyMatcher = /^(?:([-+]{1}) ?)?(?:([A-Z]{3}) ?)?(?:([^\d ]+?) ?)?(((?:\d{1,3}([,. ’'\u00A0]))*?\d{1,})(([,.])\d{1,2})?)(?: ?([^\d ]+?))??(?: ?([A-Z]{3}))?$/;
 var gr = /^\d{1,3}([,. ’'\u00A0]\d{3})*$/; // validate groups
 var ind = /^\d{1,2}(,\d{2})*(,\d{3})?$/; // exception for Indian number format
 
@@ -7,16 +7,16 @@ module.exports = function parseCurrency(priceStr) {
   priceStr = priceStr.trim();
   var match = priceStr.match(currencyMatcher);
   if (!match) return null;
-  var groupSeparator = match[7] || '';
-  var decimalSeparator = match[9] || '';
+  var groupSeparator = match[6] || '';
+  var decimalSeparator = match[8] || '';
   if (groupSeparator === decimalSeparator && decimalSeparator) {
     return null;
   }
-  var integer = match[1] == '-' ? '-' + match[6] : match[6];
-  if (groupSeparator && !match[6].match(gr) && !match[6].match(ind)) {
+  var integer = match[1] == '-' ? '-' + match[5] : match[5];
+  if (groupSeparator && !match[5].match(gr) && !match[5].match(ind)) {
     return null;
   }
-  var value = match[5];
+  var value = match[4];
   if (!value) return null;
   if (groupSeparator) {
     value = value.replace(RegExp('\\' + groupSeparator, 'g'), '');
@@ -32,9 +32,9 @@ module.exports = function parseCurrency(priceStr) {
     raw: priceStr,
     value: numericVal,
     integer: integer || '',
-    decimals: match[8] || '',
-    currency: match[3] || match[11] || '',
-    symbol: match[4] || match[10] || '',
+    decimals: match[7] || '',
+    currency: match[2] || match[10] || '',
+    symbol: match[3] || match[9] || '',
     decimalSeparator: decimalSeparator,
     groupSeparator: groupSeparator,
     sign: match[1] || ''
